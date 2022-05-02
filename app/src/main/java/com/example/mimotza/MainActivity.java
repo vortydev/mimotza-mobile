5dcb6916fb5e565155fb21a409e201ca9e37b984
IsabelleRioux <samoyede2003@gmail.com>
2022-05-02 11:05:54 -0400

    accès a loginsignin

--- app/src/main/java/com/example/mimotza/MainActivity.java
+++ app/src/main/java/com/example/mimotza/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.mimotza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Intent intentInsc = new Intent(MainActivity.this, LogInSignIn.class); // redirection vers l'écran de connexion
+        //startActivity(intentInsc);
+
+        Button isa = (Button) findViewById(R.id.isa);
+        isa.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.isa:
+                Intent intentInsc = new Intent(MainActivity.this, LogInSignIn.class);
+                startActivity(intentInsc);
+                break;
+
+            default:
+                break;
+        }
     }
 }
