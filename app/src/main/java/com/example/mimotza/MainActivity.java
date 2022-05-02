a53ffb3f6d074c0fdc3d62dcbf724523c9bd4a92
vortydev <etienne.dmenard@gmail.com>
2022-05-02 10:33:26 -0400

    created base theme

--- app/src/main/java/com/example/mimotza/MainActivity.java
+++ app/src/main/java/com/example/mimotza/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.mimotza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnJouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bouton Jouer
+        btnJouer = (Button) findViewById(R.id.buttonJouer);
+        btnJouer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonJouer:
+                Intent intentJouer = new Intent(MainActivity.this, Jeu.class);
+                intentJouer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intentJouer);
+                break;
+            default:
+                // rien :)
+                break;
+        }
     }
 }
