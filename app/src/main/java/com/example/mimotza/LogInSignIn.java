707c890df3cc795449d7d3c4af8fd3fb0d3dc1ba
IsabelleRioux <samoyede2003@gmail.com>
2022-05-02 10:39:19 -0400

    Ajout d'un bouton d'inscription et d'un bouton de connexion à LogInSignIn

---
+++ app/src/main/java/com/example/mimotza/LogInSignIn.java
@@ -0,0 +1,14 @@
+package com.example.mimotza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_sign_in);
+    }
+}
