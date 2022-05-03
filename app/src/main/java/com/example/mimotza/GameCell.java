b1bfcf51990de40daf00a7295c098783c490ab9f
vortydev <etienne.dmenard@gmail.com>
2022-05-03 16:12:26 -0400

    documentation

--- app/src/main/java/com/example/mimotza/GameCell.java
+++ app/src/main/java/com/example/mimotza/GameCell.java
@@ -2,63 +2,63 @@ package com.example.mimotza;
 
 import android.widget.TextView;
 
+/**
+ * Cellule de la grille du jeu Mi-Mot-Za.
+ * @author Étienne Ménard
+ */
 public class GameCell {
     private TextView cell;
     private int lettre;
     private CellState state;
 
-    public GameCell(TextView c) {
-        cell = c;
+    /**
+     * Constructeur de l'objet Cellule.
+     * @author Étienne Ménard
+     * @param v TextView de la cellule.
+     */
+    public GameCell(TextView v) {
+        cell = v;
         lettre = 0;
         state = CellState.DEFAULT;
 
         updateCell();
     }
 
-    public TextView getCell() {
-        return cell;
-    }
-
-    public void setCell(TextView c) {
-        cell = c;
-    }
-
-    public int getLettre() {
-        return lettre;
-    }
-
-    public void setLettre(int l) {
-        lettre = l;
-    }
-
+    /**
+     * Affiche la lettre envoyée par une touche de clavier sur la cellule.
+     * @author Étienne Ménard
+     * @param l ID du string statique de la lettre dans l'application.
+     */
     public void setKeyPress(int l) {
         lettre = l;
         setState(CellState.OCCUPIED);
         updateCell();
     }
 
+    /**
+     * Clear la lettre de la cellule.
+     * @author Étienne Ménard
+     */
     public void clearCell() {
         lettre = 0;
         setState(CellState.DEFAULT);
         updateCell();
     }
 
-    public CellState getState() {
-        return state;
-    }
-
+    /**
+     * Set l'état de la cellule, définit par l'enum CellState.
+     * @author Étienne Ménard
+     * @param s State de la cellule (DEFAULT, OCCUPIED, BAD, GOOD, VALID)
+     */
     public void setState(CellState s) {
         state = s;
     }
 
-    public void updateCell() {
-        if (lettre == 0) {
-            cell.setText("");
-        }
-        else {
-            cell.setText(lettre);
-        }
-
+    /**
+     * Met à jour le state de la cellule.
+     * @author Étienne Ménard
+     */
+    private void updateState() {
         switch (state) {
             case OCCUPIED:
                 cell.setBackgroundResource(R.drawable.cell_occupied);
@@ -76,5 +76,27 @@ public class GameCell {
                 cell.setBackgroundResource(R.drawable.cell_default);
                 break;
         }
+    }
+
+    /**
+     * Met à jour la lettre de la cellule.
+     * @author Étienne Ménard
+     */
+    private void updateLettre() {
+        if (lettre == 0) {
+            cell.setText("");
+        }
+        else {
+            cell.setText(lettre);
+        }
+    }
+
+    /**
+     * Met à jour la cellule.
+     * @author Étienne Ménard
+     */
+    private void updateCell() {
+        updateState();
+        updateLettre();
     }
 }
--- app/src/main/java/com/example/mimotza/Jeu.java
+++ app/src/main/java/com/example/mimotza/Jeu.java
@@ -3,16 +3,18 @@ package com.example.mimotza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.TextView;
 
-import java.io.Console;
 import java.util.ArrayList;
 
+/**
+ * Activité du jeu Mi-Mot-Za.
+ * @author Étienne Ménard
+ */
 public class Jeu extends AppCompatActivity {
     private GameGrid grid;
-    private DBWrapper bd;
+    private DBWrapper bdMimotza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,7 @@ public class Jeu extends AppCompatActivity {
         setContentView(R.layout.activity_jeu);
 
         // connect to database
-        bd = new DBWrapper(this, "mimotza");
+        bdMimotza = new DBWrapper(this, "mimotza");
 
         // creates grid object
         grid = new GameGrid(fetchCells());
@@ -28,84 +30,85 @@ public class Jeu extends AppCompatActivity {
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        // my god that's tedious
         switch (keyCode) {
             case KeyEvent.KEYCODE_A:
-                grid.setLetterAtPos(R.string.lettre_a);
+                grid.setLettreCellule(R.string.lettre_a);
                 return true;
             case KeyEvent.KEYCODE_B:
-                grid.setLetterAtPos(R.string.lettre_b);
+                grid.setLettreCellule(R.string.lettre_b);
                 return true;
             case KeyEvent.KEYCODE_C:
-                grid.setLetterAtPos(R.string.lettre_c);
+                grid.setLettreCellule(R.string.lettre_c);
                 return true;
             case KeyEvent.KEYCODE_D:
-                grid.setLetterAtPos(R.string.lettre_d);
+                grid.setLettreCellule(R.string.lettre_d);
                 return true;
             case KeyEvent.KEYCODE_E:
-                grid.setLetterAtPos(R.string.lettre_e);
+                grid.setLettreCellule(R.string.lettre_e);
                 return true;
             case KeyEvent.KEYCODE_F:
-                grid.setLetterAtPos(R.string.lettre_f);
+                grid.setLettreCellule(R.string.lettre_f);
                 return true;
             case KeyEvent.KEYCODE_G:
-                grid.setLetterAtPos(R.string.lettre_g);
+                grid.setLettreCellule(R.string.lettre_g);
                 return true;
             case KeyEvent.KEYCODE_H:
-                grid.setLetterAtPos(R.string.lettre_h);
+                grid.setLettreCellule(R.string.lettre_h);
                 return true;
             case KeyEvent.KEYCODE_I:
-                grid.setLetterAtPos(R.string.lettre_i);
+                grid.setLettreCellule(R.string.lettre_i);
                 return true;
             case KeyEvent.KEYCODE_J:
-                grid.setLetterAtPos(R.string.lettre_j);
+                grid.setLettreCellule(R.string.lettre_j);
                 return true;
             case KeyEvent.KEYCODE_K:
-                grid.setLetterAtPos(R.string.lettre_k);
+                grid.setLettreCellule(R.string.lettre_k);
                 return true;
             case KeyEvent.KEYCODE_L:
-                grid.setLetterAtPos(R.string.lettre_l);
+                grid.setLettreCellule(R.string.lettre_l);
                 return true;
             case KeyEvent.KEYCODE_M:
-                grid.setLetterAtPos(R.string.lettre_m);
+                grid.setLettreCellule(R.string.lettre_m);
                 return true;
             case KeyEvent.KEYCODE_N:
-                grid.setLetterAtPos(R.string.lettre_n);
+                grid.setLettreCellule(R.string.lettre_n);
                 return true;
             case KeyEvent.KEYCODE_O:
-                grid.setLetterAtPos(R.string.lettre_o);
+                grid.setLettreCellule(R.string.lettre_o);
                 return true;
             case KeyEvent.KEYCODE_P:
-                grid.setLetterAtPos(R.string.lettre_p);
+                grid.setLettreCellule(R.string.lettre_p);
                 return true;
             case KeyEvent.KEYCODE_Q:
-                grid.setLetterAtPos(R.string.lettre_q);
+                grid.setLettreCellule(R.string.lettre_q);
                 return true;
             case KeyEvent.KEYCODE_R:
-                grid.setLetterAtPos(R.string.lettre_r);
+                grid.setLettreCellule(R.string.lettre_r);
                 return true;
             case KeyEvent.KEYCODE_S:
-                grid.setLetterAtPos(R.string.lettre_s);
+                grid.setLettreCellule(R.string.lettre_s);
                 return true;
             case KeyEvent.KEYCODE_T:
-                grid.setLetterAtPos(R.string.lettre_t);
+                grid.setLettreCellule(R.string.lettre_t);
                 return true;
             case KeyEvent.KEYCODE_U:
-                grid.setLetterAtPos(R.string.lettre_u);
+                grid.setLettreCellule(R.string.lettre_u);
                 return true;
             case KeyEvent.KEYCODE_V:
-                grid.setLetterAtPos(R.string.lettre_v);
+                grid.setLettreCellule(R.string.lettre_v);
                 return true;
             case KeyEvent.KEYCODE_W:
-                grid.setLetterAtPos(R.string.lettre_w);
+                grid.setLettreCellule(R.string.lettre_w);
                 return true;
             case KeyEvent.KEYCODE_X:
-                grid.setLetterAtPos(R.string.lettre_x);
+                grid.setLettreCellule(R.string.lettre_x);
                 return true;
             case KeyEvent.KEYCODE_Y:
-                grid.setLetterAtPos(R.string.lettre_y);
+                grid.setLettreCellule(R.string.lettre_y);
                 return true;
             case KeyEvent.KEYCODE_Z:
-                grid.setLetterAtPos(R.string.lettre_z);
+                grid.setLettreCellule(R.string.lettre_z);
                 return true;
             case KeyEvent.KEYCODE_DEL:
                 grid.eraseLetterAtPos();
@@ -118,6 +121,11 @@ public class Jeu extends AppCompatActivity {
         }
     }
 
+    /**
+     * Retourne une liste de toutes les cellules de la grille.
+     * @author Étienne Ménard
+     * @return ArrayList de TextViews
+     */
     private ArrayList<TextView> fetchCells() {
         ArrayList<TextView> cells = new ArrayList<>();
 
