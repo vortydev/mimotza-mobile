76059bcda24a61010341d92fbfb4cc9fa3fac033
IsabelleRioux <samoyede2003@gmail.com>
2022-05-03 09:21:38 -0400

    ajout d'un bouton retour à l'activité inscription

--- app/src/main/java/com/example/mimotza/Inscription.java
+++ app/src/main/java/com/example/mimotza/Inscription.java
@@ -3,12 +3,38 @@ package com.example.mimotza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Inscription extends AppCompatActivity {
+public class Inscription extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        Button insc = (Button) findViewById(R.id.sub);
+        Button back = (Button) findViewById(R.id.back);
+
+        insc.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.sub:
+                //envoi de formulaire
+                break;
+
+            case R.id.back:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
 }
