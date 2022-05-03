7b187d3a184dff83a4842f202aa723058e882284
François-Nicolas Gitzhofer <fngitzhofersds@gmail.com>
2022-05-03 15:15:06 -0400

    Merge dev => fn.

--- app/src/main/java/com/example/mimotza/MainActivity.java
+++ app/src/main/java/com/example/mimotza/MainActivity.java
@@ -7,22 +7,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnJouer;
+    private Button btnForum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button boutForum = findViewById(R.id.goToForum);
+        // bouton Jouer
+        btnJouer = (Button) findViewById(R.id.buttonJouer);
+        btnForum = (Button) findViewById(R.id.goToForum);
+        btnJouer.setOnClickListener(this);
+        btnForum.setOnClickListener(this);
+    }
 
-        boutForum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(boutForum.getContext(), ForumActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                boutForum.getContext().startActivity(intent);
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonJouer:
+                Intent intentJouer = new Intent(MainActivity.this, Jeu.class);
+                intentJouer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intentJouer);
+                break;
+            case R.id.goToForum:
+                Intent intent = new Intent(MainActivity.this, ForumActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                break;
+            default:
+                // rien :)
+                break;
+        }
     }
 }
