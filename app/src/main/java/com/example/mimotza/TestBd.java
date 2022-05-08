f1b6837455e2b4c1e6f034fbfa8e44dd3e7e544c
melkuz <50109008+melkuz@users.noreply.github.com>
2022-05-08 11:04:59 -0400

    ajout de table mots non joues

--- app/src/main/java/com/example/mimotza/TestBd.java
+++ app/src/main/java/com/example/mimotza/TestBd.java
@@ -48,19 +48,17 @@ public class TestBd extends AppCompatActivity {
         temp.addColumn("idUser",DBType.INTEGER);
         temp.addColumn("win",DBType.INTEGER);
         temp.addColumn("score",DBType.INTEGER);
-        temp.addColumn("temps",DBType.TEXT);
-        temp.addColumn("dateEmission",DBType.TEXT);
-        temp.addColumn("idMot",DBType.INTEGER);
+
+
+        temp = new DBTable("motJoueur");
+        temp.addColumn("mot",DBType.INTEGER);
+        temp.addColumn("joue",DBType.INTEGER);
+        temp.addColumn("win",DBType.INTEGER);
         bd.addTable(temp);
         bd.buildContent();
 
 
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_test_bd);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
